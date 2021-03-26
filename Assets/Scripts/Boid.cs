9ada1a76f99beafab0b702055f38be7512689418
Charles <charleswonghm@gmail.com>
2021-03-26 16:32:21 -0400

    Moving cones

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = maxSpeed * transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += velocity * Time.deltaTime;
+
+        if (transform.position.x < -15 || transform.position.x > 15) {
+            transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y < -15 || transform.position.y > 15) {
+            transform.position = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z < -15 || transform.position.z > 15) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    
+    public int numBoids = 10;
+    public float spawnRadius = 10f;
+
+    public Boid boid;
+
+    // Spawn
+    void Awake()
+    {
+        for (int i = 0; i < numBoids; i++) {
+            Boid b = Object.Instantiate(boid);
+            b.transform.position = transform.position + (Random.insideUnitSphere * spawnRadius);;
+            b.transform.forward = Random.insideUnitSphere;
+        }
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(30, 30, 30));
+    }
+}
